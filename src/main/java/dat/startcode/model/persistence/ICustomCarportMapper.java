f56dea05059ddce6f42a7eed83db4876d0bc2bd8
Denis <hvorerden@hotmail.com>
2022-05-06 11:06:56 +0200

    started work on CustomCarportMappers

---
+++ src/main/java/dat/startcode/model/persistence/ICustomCarportMapper.java
@@ -0,0 +1,13 @@
+package dat.startcode.model.persistence;
+
+import dat.startcode.model.entities.Carport;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+public interface ICustomCarportMapper {
+    ArrayList<Carport> getAll = new ArrayList<>();
+
+
+
+}
