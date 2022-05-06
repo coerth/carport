e8977ac28a77d7a095b30d40e8bde8e2c4ac5e54
Morten Bendeke <mbendeke@hotmail.com>
2022-05-06 09:43:39 +0200

    lavet MaterialMapper interface

--- src/main/java/dat/startcode/model/persistence/IMaterialMapper.java
+++ src/main/java/dat/startcode/model/persistence/IMaterialMapper.java
@@ -1,7 +1,21 @@
 package dat.startcode.model.persistence;
 
+import dat.startcode.model.entities.Material;
+
+import java.util.ArrayList;
+
 public interface IMaterialMapper
 {
 
-    public Arraylist<Mate> getAllMaterials()
+    public ArrayList<Material> getAllMaterials();
+    public Material getSpecificMaterial(int materialID);
+    public boolean updatePrice(int materialId);
+    public boolean updateName(int materialId);
+    public boolean updateLength(int materialId);
+    public boolean updateUnit(int materialId);
+    public boolean deleteMaterial(int material);
+
+
+
+
 }
