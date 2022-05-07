683bace0aeeed6aa51d2046cb346a51bc56419ca
Denis <hvorerden@hotmail.com>
2022-05-07 12:06:28 +0200

    NU ER DATABASEN ENDELIG RETTET!

--- src/main/resources/sql creation fil NY.sql
+++ src/main/resources/sql creation fil NY.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema carport
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `carport` ;
@@ -26,16 +29,28 @@ CREATE TABLE IF NOT EXISTS `carport`.`account` (
   `password` VARCHAR(45) NOT NULL,
   `role` INT NULL DEFAULT '2',
   PRIMARY KEY (`account_id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb3;
 
-INSERT INTO `carport`.`account`
-(`email`,
-`password`,
-`role`)
-VALUES
-('admin@fog.dk', 1234, 1),
-('test@test.dk', 1234, 2),
-('a@a.dk', 1234, 2);
+
+-- -----------------------------------------------------
+-- Table `carport`.`carport_request`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `carport`.`carport_request` ;
+
+CREATE TABLE IF NOT EXISTS `carport`.`carport_request` (
+  `carport_request_id` INT NOT NULL AUTO_INCREMENT,
+  `width` INT NOT NULL,
+  `height` INT NOT NULL,
+  `roof` VARCHAR(45) NOT NULL,
+  `roof_incline` INT NULL DEFAULT NULL,
+  `is_approved` TINYINT NOT NULL DEFAULT 0,
+  `shed_length` INT NULL,
+  `shed_width` INT NULL,
+  PRIMARY KEY (`carport_request_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -51,25 +66,35 @@ CREATE TABLE IF NOT EXISTS `carport`.`customer` (
   `zip` INT NOT NULL,
   `mobile` INT NOT NULL,
   `account_id` INT NOT NULL,
+  `carport_request_id` INT NOT NULL,
   PRIMARY KEY (`customer_id`),
   INDEX `fk_customer_user1_idx` (`account_id` ASC) VISIBLE,
+  INDEX `fk_customer_carport_request1_idx` (`carport_request_id` ASC) VISIBLE,
   CONSTRAINT `fk_customer_user1`
     FOREIGN KEY (`account_id`)
-    REFERENCES `carport`.`account` (`account_id`)
+    REFERENCES `carport`.`account` (`account_id`),
+  CONSTRAINT `fk_customer_carport_request1`
+    FOREIGN KEY (`carport_request_id`)
+    REFERENCES `carport`.`carport_request` (`carport_request_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb3;
 
-INSERT INTO `carport`.`customer`
-(`name`,
-`address`,
-`city`,
-`zip`,
-`mobile`,
-`account_id`)
-VALUES
-('test testington', 'balladegade 42', 'Bagsværd', 2880, 12345678, 2),
-('allan albertson', 'Bygade 20', 'Søborg', 2860, 87654321, 3);
+
+-- -----------------------------------------------------
+-- Table `carport`.`description`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `carport`.`description` ;
+
+CREATE TABLE IF NOT EXISTS `carport`.`description` (
+  `description_id` INT NOT NULL AUTO_INCREMENT,
+  `description` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`description_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -81,13 +106,9 @@ CREATE TABLE IF NOT EXISTS `carport`.`material_type` (
   `type_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`type_id`))
-ENGINE = InnoDB;
-
-INSERT INTO `carport`.`material_type`
-(`name`)
-VALUES
-('Træ & Tagplader'),
-('Beslag & Skruer');
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -100,35 +121,17 @@ CREATE TABLE IF NOT EXISTS `carport`.`material` (
   `name` VARCHAR(45) NOT NULL,
   `price` INT NOT NULL,
   `unit` VARCHAR(45) NOT NULL,
-  `max_length` INT NULL,
+  `max_length` INT NULL DEFAULT NULL,
   `type_id` INT NOT NULL,
   PRIMARY KEY (`material_id`),
   INDEX `fk_material_material_type1_idx` (`type_id` ASC) VISIBLE,
   CONSTRAINT `fk_material_material_type1`
     FOREIGN KEY (`type_id`)
-    REFERENCES `carport`.`material_type` (`type_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `carport`.`material_type` (`type_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb3;
 
-INSERT INTO `carport`.`material`
-(`name`,
-`price`,
-`unit`,
-`max_length`,
-`type_id`)
-VALUES
-('25x200 mm. trykimp. Brædt', 50, 'Stk', 720, 1),
-('45x95 mm. Reglar ub.', 25, 'Stk', 720, 1);
-
-INSERT INTO `carport`.`material`
-(`name`,
-`price`,
-`unit`,
-`type_id`)
-VALUES
-('plastmo bundskruer 200 stk.', 10, 'Pakke', 2),
-('universal 190 mm højre', 5, 'Stk', 2);
 
 -- -----------------------------------------------------
 -- Table `carport`.`order`
@@ -139,88 +142,105 @@ CREATE TABLE IF NOT EXISTS `carport`.`order` (
   `order_id` INT NOT NULL AUTO_INCREMENT,
   `customer_id` INT NOT NULL,
   `date` DATETIME NOT NULL,
+  `price` INT NULL,
   PRIMARY KEY (`order_id`),
   INDEX `fk_orderline_has_customer_customer1_idx` (`customer_id` ASC) VISIBLE,
   CONSTRAINT `fk_orderline_has_customer_customer1`
     FOREIGN KEY (`customer_id`)
-    REFERENCES `carport`.`customer` (`customer_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-INSERT INTO `carport`.`order`
-(`customer_id`,
-`date`)
-VALUES
-(1, '2022-02-20 00:00:00'),
-(2, '2022-04-11 08:50:27');
-
--- -----------------------------------------------------
--- Table `carport`.`description`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `carport`.`description` ;
-
-CREATE TABLE IF NOT EXISTS `carport`.`description` (
-  `description_id` INT NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`description_id`))
-ENGINE = InnoDB;
-
-INSERT INTO `carport`.`description`
-(`description`)
-VALUES
-('understernbrædder til for & bag ende'),
-('løsholter til skur gavle'),
-('Skruer til tagplader'),
-('understernbrædder til for & bag ende'),
-('løsholter til skur gavle'),
-('Skruer til tagplader');
+    REFERENCES `carport`.`customer` (`customer_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `carport`.`orderline`
+-- Table `carport`.`bill_of_materials`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `carport`.`orderline` ;
+DROP TABLE IF EXISTS `carport`.`bill_of_materials` ;
 
-CREATE TABLE IF NOT EXISTS `carport`.`orderline` (
-  `orderline_id` INT NOT NULL AUTO_INCREMENT,
-  `material_id` INT NOT NULL,
-  `quantity` INT NOT NULL,
-  `description_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `carport`.`bill_of_materials` (
+  `bom_id` INT NOT NULL AUTO_INCREMENT,
   `order_id` INT NOT NULL,
-  PRIMARY KEY (`orderline_id`),
-  INDEX `fk_orderline_material1_idx` (`material_id` ASC) VISIBLE,
-  INDEX `fk_orderline_order1_idx` (`order_id` ASC) VISIBLE,
-  INDEX `fk_orderline_description1_idx` (`description_id` ASC) VISIBLE,
-  CONSTRAINT `fk_orderline_material1`
-    FOREIGN KEY (`material_id`)
-    REFERENCES `carport`.`material` (`material_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_orderline_order1`
+  PRIMARY KEY (`bom_id`),
+  INDEX `fk_bill_of_materials_order1_idx` (`order_id` ASC) VISIBLE,
+  CONSTRAINT `fk_bill_of_materials_order1`
     FOREIGN KEY (`order_id`)
     REFERENCES `carport`.`order` (`order_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_orderline_description1`
-    FOREIGN KEY (`description_id`)
-    REFERENCES `carport`.`description` (`description_id`)
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `carport`.`shed`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `carport`.`shed` ;
+
+CREATE TABLE IF NOT EXISTS `carport`.`shed` (
+  `shed_id` INT NOT NULL AUTO_INCREMENT,
+  `bom_id` INT NOT NULL,
+  PRIMARY KEY (`shed_id`),
+  INDEX `fk_shed_bill_of_materials1_idx` (`bom_id` ASC) VISIBLE,
+  CONSTRAINT `fk_shed_bill_of_materials1`
+    FOREIGN KEY (`bom_id`)
+    REFERENCES `carport`.`bill_of_materials` (`bom_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `carport`.`carport`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `carport`.`carport` ;
+
+CREATE TABLE IF NOT EXISTS `carport`.`carport` (
+  `carport_id` INT NOT NULL AUTO_INCREMENT,
+  `bom_id` INT NOT NULL,
+  PRIMARY KEY (`carport_id`),
+  INDEX `fk_carport_bill_of_materials1_idx` (`bom_id` ASC) VISIBLE,
+  CONSTRAINT `fk_carport_bill_of_materials1`
+    FOREIGN KEY (`bom_id`)
+    REFERENCES `carport`.`bill_of_materials` (`bom_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-INSERT INTO `carport`.`orderline`
-(`material_id`,
-`quantity`,
-`description_id`,
-`order_id`)
-VALUES
-(1, 20, 1, 1),
-(2, 5, 2, 1),
-(3, 2, 3, 1),
-(1, 10, 4, 2),
-(2, 2, 5, 2),
-(3, 1, 6, 2);
+
+-- -----------------------------------------------------
+-- Table `carport`.`bomline`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `carport`.`bomline` ;
+
+CREATE TABLE IF NOT EXISTS `carport`.`bomline` (
+  `bomline_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description_id` INT NOT NULL,
+  `bom_id` INT NOT NULL,
+  `material_id` INT NOT NULL,
+  PRIMARY KEY (`bomline_id`),
+  INDEX `fk_bomline_description1_idx` (`description_id` ASC) VISIBLE,
+  INDEX `fk_bomline_bill_of_materials1_idx` (`bom_id` ASC) VISIBLE,
+  INDEX `fk_bomline_material1_idx` (`material_id` ASC) VISIBLE,
+  CONSTRAINT `fk_bomline_description1`
+    FOREIGN KEY (`description_id`)
+    REFERENCES `carport`.`description` (`description_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_bomline_bill_of_materials1`
+    FOREIGN KEY (`bom_id`)
+    REFERENCES `carport`.`bill_of_materials` (`bom_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_bomline_material1`
+    FOREIGN KEY (`material_id`)
+    REFERENCES `carport`.`material` (`material_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
