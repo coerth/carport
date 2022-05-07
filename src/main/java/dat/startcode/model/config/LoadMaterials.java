630e980ef1dff50516a525dab74862c812addbf4
Morten Bendeke <mbendeke@hotmail.com>
2022-05-07 12:20:34 +0200

    mere weblistener

--- src/main/java/dat/startcode/model/config/LoadMaterials.java
+++ src/main/java/dat/startcode/model/config/LoadMaterials.java
@@ -1,7 +1,9 @@
 package dat.startcode.model.config;
 
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class LoadMaterials implements ServletContextListener
 {
 
