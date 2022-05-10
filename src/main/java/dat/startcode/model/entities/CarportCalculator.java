8bc1aed1c89bb0afe4ade71f2bb3628d465cf3c0
Yelong He <Yelong.he@hotmail.com>
2022-05-10 15:19:54 +0200

    udregning af stolper og udregning af bolte til stolper lavet

--- src/main/java/dat/startcode/model/entities/CarportCalculator.java
+++ src/main/java/dat/startcode/model/entities/CarportCalculator.java
@@ -1,7 +1,42 @@
 package dat.startcode.model.entities;
 
-public class CarportCalculator
-{
+public class CarportCalculator {
 
 
+    public int calculatePostAmount(int carportLength) {
+
+        int minDistanceBetweenPost = 250;
+        int maxDistanceBetweenPost = 310;
+        int minDistanceFromStern = 45;
+        int maxDistanceFromStern = 100;
+        int postDistance = 0;
+        int post = 0;
+
+
+        if (minDistanceFromStern == 45 && maxDistanceFromStern == 100) {
+            postDistance = carportLength - minDistanceFromStern;
+            postDistance = postDistance / 2;
+
+            if (postDistance < minDistanceBetweenPost) {
+                post = 0;
+            } else if (postDistance > minDistanceBetweenPost && postDistance < maxDistanceBetweenPost) {
+                post = 1;
+            } else if (postDistance > maxDistanceBetweenPost) {
+                post = 2;
+            }
+
+        }
+
+        return post * 2 + 4;
+
+    }
+
+    public int calculateCarriageBolt(int carportLength) {
+
+        int carriageBolt = 2;
+
+        carriageBolt = carriageBolt * calculatePostAmount(carportLength);
+
+        return carriageBolt;
+    }
 }
---
+++ src/test/java/dat/startcode/model/entities/CarportCalculatorTest.java
@@ -0,0 +1,27 @@
+package dat.startcode.model.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarportCalculatorTest {
+CarportCalculator carportCalculator = new CarportCalculator();
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void calculatePostAmountTest() {
+        int post = carportCalculator.calculatePostAmount(600);
+        assertEquals(6,post);
+    }
+
+    @Test
+    void calculateCarriageBolt(){
+        int carriageBolt = carportCalculator.calculateCarriageBolt(600);
+        assertEquals(12, carriageBolt);
+    }
+
+}
