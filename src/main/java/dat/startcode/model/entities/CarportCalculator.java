770c5e6750644ffb4ec7fec5ca5a1d423162e432
Denis <hvorerden@hotmail.com>
2022-05-12 10:24:31 +0200

    new funny calculators, morethanoneneeded and weatherboards

--- src/main/java/dat/startcode/model/entities/CarportCalculator.java
+++ src/main/java/dat/startcode/model/entities/CarportCalculator.java
@@ -2,57 +2,71 @@ package dat.startcode.model.entities;
 
 import java.util.ArrayList;
 
-public class CarportCalculator
-{
+public class CarportCalculator {
 
 
     public Material calculateMaterialLength(int dimension, ArrayList<Material> listOfMaterials) {
 
         for (Material material : listOfMaterials) {
-            if(material.getLength() > dimension) {
+            if (material.getLength() > dimension) {
                 return material;
             }
         }
         return null;
     }
 
-    public int calculateQuantityOfBoxes(int amountToBeUsed,Material material) {
+    public ArrayList calculateMaterialIfMoreThanOneIsNeeded(int dimension, ArrayList<Material> listOfMaterials) {
+
+        ArrayList<Material> materialArrayList = null;
+        Material material1 = null;
+        Material material2 = null;
+
+        for (int i = 0; i < listOfMaterials.size(); i++) {
+            material1 = listOfMaterials.get(i);
+            for (int j = 0; j < listOfMaterials.size(); j++) {
+                material2 = listOfMaterials.get(j);
+                if (material1.getLength() + material2.getLength() > dimension) {
+                    break;
+                }
+            }
+            if (material1.getLength() + material2.getLength() > dimension) {
+                break;
+            }
+        }
+        materialArrayList.add(material1);
+        materialArrayList.add(material2);
+        return materialArrayList;
+    }
+
+
+    public int calculateQuantityOfBoxes(int amountToBeUsed, Material material) {
 
         int amount = 1;
 
-        while(amountToBeUsed < material.getQuantity() * amount) {
+        while (amountToBeUsed < material.getQuantity() * amount) {
             amount++;
         }
         return amount;
     }
 
 
-
-
-    public ArrayList<Bomline> calculateRoofPlates(int carportLength, int carportWidth, ArrayList<Material> listOfRoofPlates)
-    {
+    public ArrayList<Bomline> calculateRoofPlates(int carportLength, int carportWidth, ArrayList<Material> listOfRoofPlates) {
         ArrayList<Bomline> bomLineArrayList = new ArrayList<Bomline>();
-        Material primaryRoofPlate =  calculateMaterialLength(carportLength, listOfRoofPlates);
+        Material primaryRoofPlate = calculateMaterialLength(carportLength, listOfRoofPlates);
 
-        if(primaryRoofPlate != null)
-        {
-          int amount = calculateAmountOfRoofPlatesForWidth(primaryRoofPlate, carportWidth);
-          bomLineArrayList.add(new Bomline("tagplader monteres på spær",primaryRoofPlate, amount));
-        }
-        else
-        {
-            primaryRoofPlate = new Material(1,"tagplade",110, "stk", 600, 109, 1, 1);
+        if (primaryRoofPlate != null) {
+            int amount = calculateAmountOfRoofPlatesForWidth(primaryRoofPlate, carportWidth);
+            bomLineArrayList.add(new Bomline("tagplader monteres på spær", primaryRoofPlate, amount));
+        } else {
+            primaryRoofPlate = new Material(1, "tagplade", 110, "stk", 600, 109, 1, 1);
             Material secondaryRoofPlate = null;
-            for(Material m : listOfRoofPlates)
-            {
-                if(primaryRoofPlate.getLength() + m.getLength() > carportLength)
-                {
+            for (Material m : listOfRoofPlates) {
+                if (primaryRoofPlate.getLength() + m.getLength() > carportLength) {
                     secondaryRoofPlate = m;
                     break;
                 }
             }
-            if(secondaryRoofPlate == null)
-            {
+            if (secondaryRoofPlate == null) {
                 throw new ArithmeticException("Kunne ikke lave en lang nok tagplade");
             }
             int amount = calculateAmountOfRoofPlatesForWidth(primaryRoofPlate, carportWidth);
@@ -61,32 +75,23 @@ public class CarportCalculator
 
         }
 
-       if(carportLength > primaryRoofPlate.getLength())
-       {
-           for(Material m : listOfRoofPlates)
-           {
-               if(primaryRoofPlate.getLength() + m.getLength() > carportLength)
-               {
+        if (carportLength > primaryRoofPlate.getLength()) {
+            for (Material m : listOfRoofPlates) {
+                if (primaryRoofPlate.getLength() + m.getLength() > carportLength) {
 
-               }
-           }
-       }
+                }
+            }
+        }
         return bomLineArrayList;
     }
 
 
-    public int calculateAmountOfRoofPlatesForWidth(Material material, int carportWidth)
-    {
-        if(material.getWidth() > carportWidth)
-        {
+    public int calculateAmountOfRoofPlatesForWidth(Material material, int carportWidth) {
+        if (material.getWidth() > carportWidth) {
             return 1;
-        }
-
-        else
-        {
+        } else {
             int amount = 1;
-            while(material.getWidth() * amount < carportWidth )
-            {
+            while (material.getWidth() * amount < carportWidth) {
                 amount++;
             }
             return amount;
@@ -101,23 +106,65 @@ public class CarportCalculator
 
     public Bomline calculateAmountOfBoxesOfBottomScrews(int quantity, Material material, int carportWidth, int carportLength) {
 
-        int screwsNeeded = calculateBottomScrewForRoof(carportWidth,carportLength);
-        int boxesNeeded = calculateQuantityOfBoxes(screwsNeeded,material);
+        int screwsNeeded = calculateBottomScrewForRoof(carportWidth, carportLength);
+        int boxesNeeded = calculateQuantityOfBoxes(screwsNeeded, material);
 
-        Bomline bomline = new Bomline("Skruer til tagplader",material,boxesNeeded);
+        Bomline bomline = new Bomline("Skruer til tagplader", material, boxesNeeded);
         return bomline;
     }
 
-    public Bomline calculateFrontAndBackSternLength (ArrayList <Material> frontAndBackSternArrayList, int rafterLength) {
-        Material backAndFrontStern = calculateMaterialLength(rafterLength +5,frontAndBackSternArrayList);
-        Bomline bomline = new Bomline("oversternbrædder til forenden",backAndFrontStern,2);
+    public Bomline calculateFrontAndBackSternLength(ArrayList<Material> frontAndBackSternArrayList, int rafterLength) {
+        Material backAndFrontStern = calculateMaterialLength(rafterLength + 5, frontAndBackSternArrayList);
+        Bomline bomline = new Bomline("oversternbrædder til forenden", backAndFrontStern, 2);
         return bomline;
     }
 
-    public Bomline calculateSideStern (ArrayList<Material> sideSternArrayList, int carportLength) {
+    public Bomline calculateSideStern(ArrayList<Material> sideSternArrayList, int carportLength) {
         Material sideStern = calculateMaterialLength(carportLength, sideSternArrayList);
-        Bomline bomline = new Bomline("oversternbrædder til siderne", sideStern,2);
+        Bomline bomline = new Bomline("oversternbrædder til siderne", sideStern, 2);
         return bomline;
+    }
+
+    public ArrayList<Bomline> calculateWeatherBoardForSide(ArrayList<Material> weatherBoardArrayList, int carportLength) {
+
+        ArrayList<Bomline> bomlineArrayList = new ArrayList<>();
+        Material sideWeatherBoard = calculateMaterialLength(carportLength, weatherBoardArrayList);
+        if (sideWeatherBoard == null) {
+
+            ArrayList<Material> sideWeatherBoardList = calculateMaterialIfMoreThanOneIsNeeded(carportLength, weatherBoardArrayList);
+
+            for (Material material : sideWeatherBoardList) {
+
+                Bomline bomline = new Bomline("vandbrædt på stern i sider", material, 2);
+                bomlineArrayList.add(bomline);
+
+            }
+        } else {
+            Bomline bomline = new Bomline("vandbrædt på stern i sider", sideWeatherBoard, 2);
+            bomlineArrayList.add(bomline);
+        }
+        return bomlineArrayList;
+    }
+
+    public ArrayList<Bomline> calculateWeatherBoardForFrontAndBack(ArrayList<Material> weatherBoardArrayList, int rafterLength) {
+
+        ArrayList<Bomline> bomlineArrayList = new ArrayList<>();
+        Material frontAndSideWeatherBoard = calculateMaterialLength(rafterLength + 5, weatherBoardArrayList);
+        if (frontAndSideWeatherBoard == null) {
+
+            ArrayList<Material> frontAndBackWeatherBoardList = calculateMaterialIfMoreThanOneIsNeeded(rafterLength, weatherBoardArrayList);
+
+            for (Material material : frontAndBackWeatherBoardList) {
+
+                Bomline bomline = new Bomline("vandbrædt på stern i forende", material, 2);
+                bomlineArrayList.add(bomline);
+
+            }
+        } else {
+            Bomline bomline = new Bomline("vandbrædt på stern i forende", frontAndSideWeatherBoard, 2);
+            bomlineArrayList.add(bomline);
+        }
+        return bomlineArrayList;
     }
 
 }
