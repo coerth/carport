a24b4df71649f9b39ec408f011c189ffdb3f39fe
Denis <hvorerden@hotmail.com>
2022-05-18 14:13:42 +0200

    deleteCarportRequest

--- src/main/java/dat/startcode/model/persistence/ICarportRequestMapper.java
+++ src/main/java/dat/startcode/model/persistence/ICarportRequestMapper.java
@@ -1,17 +1,17 @@
 package dat.startcode.model.persistence;
 
 import dat.startcode.model.entities.CarportRequest;
-import dat.startcode.model.entities.Material;
 import dat.startcode.model.exceptions.DatabaseException;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface ICustomCarportMapper {
+public interface ICarportRequestMapper {
     ArrayList<CarportRequest> getAll = new ArrayList<>();
     CarportRequest createCarportRequest(int width, int length, String roofType, int roofIncline, int shedWidth, int shedLength, int customerId ) throws DatabaseException, SQLException;
     CarportRequest getSpecificRequest(int carportRequestId);
     ArrayList<CarportRequest> getAllRequests();
+    boolean deleteCarportRequest(int carportRequestId);
 
 
 }
--- src/main/java/dat/startcode/model/services/CarportRequestFacade.java
+++ src/main/java/dat/startcode/model/services/CarportRequestFacade.java
@@ -1,13 +1,9 @@
 package dat.startcode.model.services;
 
 import dat.startcode.model.entities.CarportRequest;
-import dat.startcode.model.entities.Customer;
-import dat.startcode.model.entities.Material;
 import dat.startcode.model.exceptions.DatabaseException;
 import dat.startcode.model.persistence.ConnectionPool;
-import dat.startcode.model.persistence.CustomCarportMapper;
-import dat.startcode.model.persistence.CustomerMapper;
-import dat.startcode.model.persistence.MaterialMapper;
+import dat.startcode.model.persistence.CarportRequestRequestMapper;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -16,20 +12,20 @@ public class CarportRequestFacade {
 
     public static CarportRequest createCarportRequest(int width, int length, String roofType, int roofIncline, int shedWidth, int shedLength, int customerId, ConnectionPool connectionPool ) throws DatabaseException, SQLException {
 
-        CustomCarportMapper customCarportMapper = new CustomCarportMapper(connectionPool);
-        return customCarportMapper.createCarportRequest(width,length,roofType,roofIncline, shedWidth,shedLength, customerId);
+        CarportRequestRequestMapper carportRequestMapper = new CarportRequestRequestMapper(connectionPool);
+        return carportRequestMapper.createCarportRequest(width,length,roofType,roofIncline, shedWidth,shedLength, customerId);
 
     }
 
     public static ArrayList<CarportRequest> getAllCarportRequests(ConnectionPool connectionPool){
-        CustomCarportMapper customCarportMapper = new CustomCarportMapper(connectionPool);
-        return customCarportMapper.getAllRequests();
+        CarportRequestRequestMapper carportRequestMapper = new CarportRequestRequestMapper(connectionPool);
+        return carportRequestMapper.getAllRequests();
     }
 
     public static CarportRequest getSpecificCarportRequest(int requestId, ConnectionPool connectionPool){
-        CustomCarportMapper customCarportMapper = new CustomCarportMapper(connectionPool);
+        CarportRequestRequestMapper carportRequestMapper = new CarportRequestRequestMapper(connectionPool);
 
-        return customCarportMapper.getSpecificRequest(requestId);
+        return carportRequestMapper.getSpecificRequest(requestId);
     }
 
 
