ceebf81e5fe2df1412ea6410476e49013b6545e8
Morten Bendeke <mbendeke@hotmail.com>
2022-05-23 13:39:49 +0200

    mere kunde, sur betul

--- src/main/java/dat/startcode/control/CustomerProfile.java
+++ src/main/java/dat/startcode/control/CustomerProfile.java
@@ -1,17 +1,17 @@
-//package dat.startcode.control;
-//
-//import dat.startcode.model.exceptions.DatabaseException;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//public class CustomerProfile extends Command {
-//
-////
-////    @Override
-////    String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
-////
-////
-////        int accountId = Integer.parseInt(request.getParameter("account_id"));
-////    }
-//}
+package dat.startcode.control;
+
+import dat.startcode.model.exceptions.DatabaseException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomerProfile extends Command {
+
+
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
+
+        return "customerprofile";
+    }
+
+}
