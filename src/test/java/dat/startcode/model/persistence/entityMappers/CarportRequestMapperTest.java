ecec20036c8416588eec3b7a375874fd30d1395c
Yelong He <Yelong.he@hotmail.com>
2022-05-28 12:50:03 +0200

    getAll test

--- src/test/java/dat/startcode/model/persistence/entityMappers/CarportRequestMapperTest.java
+++ src/test/java/dat/startcode/model/persistence/entityMappers/CarportRequestMapperTest.java
@@ -1,7 +1,11 @@
 package dat.startcode.model.persistence.entityMappers;
 
+import dat.startcode.model.entities.CarportRequest;
+import dat.startcode.model.entities.Material;
+import dat.startcode.model.exceptions.DatabaseException;
 import dat.startcode.model.persistence.ConnectionPool;
 import dat.startcode.model.services.CarportRequestFacade;
+import dat.startcode.model.services.MaterialFacade;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,7 +37,7 @@ class CarportRequestMapperTest {
                 statement.execute("delete from `account`");
                 statement.execute("alter table account auto_increment=0");
 
-                statement.execute("INSERT INTO `carport_request` (1,600,780,'Plasttrapez',null,false,280,600,2);");
+                statement.execute("INSERT INTO `carport`.`carport_request`(`carport_request_id`,`width`,`length`,`roof`,`roof_incline`,`is_approved`,`shed_length`,`shed_width`,`customer_id`) VALUES(1,600,780,'Plasttrapez',null,false,280,600,2)");
             }
 
         } catch (SQLException throwables) {
@@ -42,12 +46,26 @@ class CarportRequestMapperTest {
     }
 
     @Test
-    void createCarportRequest() {
-        CarportRequestFacade carportRequestFacade;
+    void testConnection() throws SQLException
+    {
+        Connection connection = connectionPool.getConnection();
+        assertNotNull(connection);
+        if (connection != null)
+        {
+            connection.close();
+        }
+    }
+
+    @Test
+    void createCarportRequest() throws SQLException, DatabaseException {
+        CarportRequest carportRequest = CarportRequestFacade.createCarportRequest(600,780,"Plasttrapez",0,600,280,1,connectionPool);
+
     }
 
     @Test
     void getSpecificRequest() {
+        CarportRequest carportRequest = CarportRequestFacade.getSpecificCarportRequest(1,connectionPool);
+        assertEquals(1,carportRequest.getRequestId());
     }
 
     @Test
