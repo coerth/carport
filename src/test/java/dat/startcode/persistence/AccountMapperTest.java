91b43c3277e3ed25c1d1ee9dbab22a666434e1a1
Morten Bendeke <mbendeke@hotmail.com>
2022-05-28 13:15:10 +0200

    accountmapper test er lavet

--- src/test/java/dat/startcode/persistence/AccountMapperTest.java
+++ src/test/java/dat/startcode/persistence/AccountMapperTest.java
@@ -12,14 +12,15 @@ import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountMapperTest
 {
-    private final static String USER = "root";
-    private final static String PASSWORD = System.getenv("dbpassword");
-    private final static String URL = "jdbc:mysql://localhost:3306/carport_test?serverTimezone=CET&allowPublicKeyRetrieval=true&useSSL=false";
+    private final static String USER = "test";
+    private final static String PASSWORD = "nemt";
+    private final static String URL = "jdbc:mysql://localhost:3306/carport_test";
 
     private static ConnectionPool connectionPool;
 
@@ -35,9 +36,11 @@ class AccountMapperTest
             try (Statement stmt = testConnection.createStatement() ) {
                 // Remove all rows from all tables
                 stmt.execute("delete from account");
+                stmt.execute("ALTER TABLE account AUTO_INCREMENT = 0");
+
                 // Indsæt et par brugere
                 stmt.execute("insert into account (email, password, role) " +
-                        "values ('user','1234','user'),('admin','1234','admin'), ('ben','1234','user')");
+                        "values ('user@fog.dk','1234',2),('admin@fog.dk','1234',1), ('ben@ben.dk','1234',2)");
             }
         } catch (SQLException throwables) {
             System.out.println(throwables.getMessage());
@@ -59,15 +62,26 @@ class AccountMapperTest
     @Test
     void login() throws DatabaseException
     {
-        Account expectedAccount = new Account("a@a.dk","1234",2);
-        Account actualAccount = AccountFacade.login("a@a.dk","1234", connectionPool);
-        assertEquals(expectedAccount, actualAccount);
+        //Account expectedAccount = new Account("a@a.dk","1234",2);
+        Account actualAccount = AccountFacade.login("admin@fog.dk","1234", connectionPool);
+        assertEquals("admin@fog.dk", actualAccount.getEmail());
     }
 
     @Test
     void invalidPasswordLogin() throws DatabaseException
     {
         assertThrows(DatabaseException.class, () -> AccountFacade.login("a@a.dk","123", connectionPool));
+    }
+
+    @Test
+    void getAccountIdTest() throws DatabaseException
+    {
+        int accountId = AccountFacade.getAccountId("admin@fog.dk", "1234", connectionPool);
+        assertEquals(2, accountId);
+
+        accountId = AccountFacade.getAccountId("ben@ben.dk", "1234", connectionPool);
+        assertEquals(3, accountId);
+
     }
 
     @Test
@@ -79,11 +93,22 @@ class AccountMapperTest
     @Test
     void createAccount() throws DatabaseException
     {
-        int newAccount = AccountFacade.createAccount("jill", "1234", 2, connectionPool);
-        Account logInAccount = AccountFacade.login("jill","1234", connectionPool);
-        Account expectedAccount = new Account("jill", "1234", 2);
-        assertEquals(expectedAccount, newAccount);
-        assertEquals(expectedAccount, logInAccount);
+        int newAccount = AccountFacade.createAccount("jill@jill.dk", "1234", 2, connectionPool);
+        assertEquals(4, newAccount);
 
+        Account account = AccountFacade.login("jill@jill.dk", "1234", connectionPool);
+        assertEquals("jill@jill.dk", account.getEmail());
+
+    }
+
+    @Test
+    void updateAccountTest() throws  DatabaseException
+    {
+        Account account = new Account("ben@ben.dk", "12345", 2, 3);
+       boolean testBoolean = AccountFacade.updateAccount(account, connectionPool);
+        assertEquals(true, testBoolean);
+
+       account = AccountFacade.login("ben@ben.dk", "12345", connectionPool);
+        assertEquals("12345", account.getPassword());
     }
 }
