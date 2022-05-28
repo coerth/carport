84e77a518d3ffa89e5872957ea918bde4b5c186f
Yelong He <Yelong.he@hotmail.com>
2022-05-28 11:56:17 +0200

    mere på carport_requestMapperTest lavet

--- src/test/java/dat/startcode/model/persistence/entityMappers/CarportRequestMapperTest.java
+++ src/test/java/dat/startcode/model/persistence/entityMappers/CarportRequestMapperTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,22 +25,38 @@ class CarportRequestMapperTest {
 
     @BeforeEach
     void setUp() {
-        try(Connection testConnection = connectionPool.getConnection()){
-            try(Statement statement = testConnection.createStatement()){
+        try (Connection testConnection = connectionPool.getConnection()) {
+            try (Statement statement = testConnection.createStatement()) {
+                //Remove all rows from table
                 statement.execute("delete from `carport_request`");
 
+                //Inserting data into table
                 statement.execute("INSERT INTO `carport_request` (1,600,780,'Plasttrapez',null,false,280,600,2);");
             }
 
+        } catch (SQLException throwables) {
+            System.out.println(throwables.getMessage());
+            fail("Database connection failed");
+        }
+    }
+
+    @Test
+    void testConnection() throws SQLException {
+        Connection connection = connectionPool.getConnection();
+        assertNotNull(connection);
+        if (connection != null) {
+            connection.close();
         }
     }
 
     @Test
     void createCarportRequest() {
+
     }
 
     @Test
     void getSpecificRequest() {
+
     }
 
     @Test
